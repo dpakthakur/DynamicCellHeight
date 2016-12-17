ca544281f531b61e8b70f3922293dfe5a8904c81
dpakthakur <deepak4218@yahoo.com>
2016-12-17 19:14:43 +0530

    Initial Commit

---
+++ DynamicCellHeight/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DynamicCellHeight
+//
+//  Created by Deepak Thakur on 17/12/16.
+//  Copyright © 2016 Zensar Technologies. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
